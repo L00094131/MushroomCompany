b39ed549ac0685845f28c22de87ba533ddbf039f
L00094131 <L00094131@student.lyit.ie>
2017-08-29 12:12:00 +0100

    Fixed issue with pom.xml war.

--- src/main/java/mushroom/spring/config/MvcConfiguration.java
+++ src/main/java/mushroom/spring/config/MvcConfiguration.java
@@ -8,12 +8,14 @@ import mushroom.spring.dao.CustomerDAOImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
 
 @Configuration
 @ComponentScan(basePackages="mushroom.spring")
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -61,11 +61,11 @@
 					<li>Letterkenny</li>
 					<li>Co. Donegal</li>
 					<li>Ireland</li>
+					<li>Tel: (074) 91 058123</li>
 				</ul>
 				<iframe
 					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2291.280075589289!2d-7.739189584363695!3d54.95064778034645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x485f94ccc1511609%3A0x94ae4d838a4b6d17!2sMain+Street!5e0!3m2!1sen!2sie!4v1502550701292"
-					width="300" height="225" frameborder="0" style="border: 0"
-					allowfullscreen> </iframe>
+					width="300" height="225" frameborder="0" style="border: 0" allowfullscreen> </iframe>
 				<br>
 				<h2>Why choose us?</h2>
 				<ul>
