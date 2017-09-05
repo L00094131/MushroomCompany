4a87aae9e02937cee42914aae05de41d32043336
L00094131 <L00094131@student.lyit.ie>
2017-09-05 12:03:12 +0100

    Added contact class including getters and setters.

--- src/main/java/mushroom/spring/model/Customer.java
+++ src/main/java/mushroom/spring/model/Customer.java
@@ -19,24 +19,33 @@ public class Customer {
 	private String customer_postcode;
 	private String customer_type;
 
-	
 	public Customer() {
 	}
-		
+
 	/**
-	 * This constructs a customer with specified first name, last name, email, address
-	 * phone number, password, postcode and type.
+	 * This constructs a customer with specified first name, last name, email,
+	 * address phone number, password, postcode and type.
 	 * 
-	 * @param customer_first_name the customers first name
-	 * @param customer_last_name the customers last name
-	 * @param customer_email the customers email
-	 * @param customer_address the customers address
-	 * @param customer_phone_number the customers phone number
-	 * @param customer_password the customers password
-	 * @param customer_postcode the customers postcode
-	 * @param customer_type the type of the customer 
+	 * @param customer_first_name
+	 *            the customers first name
+	 * @param customer_last_name
+	 *            the customers last name
+	 * @param customer_email
+	 *            the customers email
+	 * @param customer_address
+	 *            the customers address
+	 * @param customer_phone_number
+	 *            the customers phone number
+	 * @param customer_password
+	 *            the customers password
+	 * @param customer_postcode
+	 *            the customers postcode
+	 * @param customer_type
+	 *            the type of the customer
 	 */
-	public Customer(String customer_first_name, String customer_last_name, String customer_email, String customer_address, String customer_phone_number, String customer_password, String customer_postcode, String customer_type) {
+	public Customer(String customer_first_name, String customer_last_name, String customer_email,
+			String customer_address, String customer_phone_number, String customer_password, String customer_postcode,
+			String customer_type) {
 		this.customer_first_name = customer_first_name;
 		this.customer_last_name = customer_last_name;
 		this.customer_email = customer_email;
@@ -46,7 +55,6 @@ public class Customer {
 		this.customer_postcode = customer_postcode;
 		this.customer_type = customer_type;
 	}
-
 
 	/**
 	 * This returns the customer ID of this customer
@@ -60,7 +68,8 @@ public class Customer {
 	/**
 	 * This sets the customer ID of this customer
 	 * 
-	 * @param customer_id the customers ID 
+	 * @param customer_id
+	 *            the customers ID
 	 */
 	public void setCustomer_id(int customer_id) {
 		this.customer_id = customer_id;
@@ -78,7 +87,8 @@ public class Customer {
 	/**
 	 * This sets the customer first name of this customer
 	 * 
-	 * @param customer_first_name the customers first name
+	 * @param customer_first_name
+	 *            the customers first name
 	 */
 	public void setCustomer_first_name(String customer_first_name) {
 		this.customer_first_name = customer_first_name;
@@ -96,7 +106,8 @@ public class Customer {
 	/**
 	 * This sets the customer last name of this customer
 	 * 
-	 * @param customer_last_name the customers last name
+	 * @param customer_last_name
+	 *            the customers last name
 	 */
 	public void setCustomer_last_name(String customer_last_name) {
 		this.customer_last_name = customer_last_name;
@@ -114,7 +125,8 @@ public class Customer {
 	/**
 	 * This sets the customer email of this customer
 	 * 
-	 * @param customer_email the customers email
+	 * @param customer_email
+	 *            the customers email
 	 */
 	public void setCustomer_email(String customer_email) {
 		this.customer_email = customer_email;
@@ -132,7 +144,8 @@ public class Customer {
 	/**
 	 * This sets the customer address of this customer
 	 * 
-	 * @param customer_address the customers address
+	 * @param customer_address
+	 *            the customers address
 	 */
 	public void setCustomer_address(String customer_address) {
 		this.customer_address = customer_address;
@@ -150,7 +163,8 @@ public class Customer {
 	/**
 	 * This sets the customer phone number of this customer
 	 * 
-	 * @param customer_phone_number the customers phone number
+	 * @param customer_phone_number
+	 *            the customers phone number
 	 */
 	public void setCustomer_phone_number(String customer_phone_number) {
 		this.customer_phone_number = customer_phone_number;
@@ -168,7 +182,8 @@ public class Customer {
 	/**
 	 * This sets the customer password of this customer
 	 * 
-	 * @param customer_password the customers password
+	 * @param customer_password
+	 *            the customers password
 	 */
 	public void setCustomer_password(String customer_password) {
 		this.customer_password = customer_password;
@@ -186,7 +201,8 @@ public class Customer {
 	/**
 	 * This sets the customer postcode of this customer
 	 * 
-	 * @param customer_postcode the customers postcode
+	 * @param customer_postcode
+	 *            the customers postcode
 	 */
 	public void setCustomer_postcode(String customer_postcode) {
 		this.customer_postcode = customer_postcode;
@@ -204,10 +220,10 @@ public class Customer {
 	/**
 	 * This sets the customer type of the customer
 	 * 
-	 * @param customer_type the customer type
+	 * @param customer_type
+	 *            the customer type
 	 */
 	public void setCustomer_type(String customer_type) {
 		this.customer_type = customer_type;
 	}
 }
-
